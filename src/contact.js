edecb3cdceca1909c7fd09e7ab46c711f4580e8e
mofcplcls <zbrogdom@gmail.com>
2020-09-02 16:43:01 +0200

    Add descriptions
    All temporary descriptions have been replaced.

--- src/contact.js
+++ src/contact.js
@@ -1,22 +1,59 @@
+const autoprefixer = require("autoprefixer");
+
+const background = document.querySelector(".background");
 
 const form = document.querySelector("form");
 
+const response_success = document.querySelector(".response-success");
+const response_failed = document.querySelector(".response-failed");
+
+response_success.remove();
+response_failed.remove();
+
+const background_height = background.offsetHeight;
+console.log(background_height);
+
 form.addEventListener("submit",async e => 
 {
+    form.remove();
     e.preventDefault();
 
-    const response = await fetch("https://postoffice.zbrogdom.pl",
+    try
     {
-        headers: {"Content-type": "application/json; charset=UTF-8"},
-        method: "post", 
-        body: JSON.stringify({
-            name: form.name.value,
-            sender: form.sender.value,
-            subject: form.subject.value,
-            msg: form.msg.value
-        })
-    });
+        const response = await fetch("https://postoffice.zbrogdo.pl",
+        {
+            headers: {"Content-type": "application/json; charset=UTF-8"},
+            method: "post", 
+            body: JSON.stringify({
+                name: form.name.value,
+                sender: form.sender.value,
+                subject: form.subject.value,
+                msg: form.msg.value
+            })
+        });
 
-    const data = await response.json();
-    console.log(data);
+        const data = await response.json();
+
+        if(!response.ok || data.error)
+        {
+            throw new Error(response.statusText);
+        }
+        else
+        {
+            background.appendChild(response_success);
+        }
+    }
+    catch(err)
+    {
+        background.classList.add("small");
+        background.appendChild(response_failed);
+        setTimeout( () => 
+        {
+            response_failed.classList.add("show");
+        },500);
+    }
+    
+
+
+    
 })
